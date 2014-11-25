e77c7220e885cab20d23bc136ba5d8f4a02f11cb
Alehssandro Emanuel <alesefs@hotmail.com>
2014-11-25 15:26:58 -0300

    Update contato.php

--- contato.php
+++ contato.php
@@ -4,18 +4,16 @@ $email=$_POST['email'];
 $titulo=$_POST['titulo'];
 $texto=$_POST['texto'];
 
-$Destinatario="alesefs@gmail.com";
+$Destinatario = "alesefs@hotmail.com";
 
-$Titulo="$titulo";
-
-$mensagem1="
+$mensagem1= "
 Uma mensagem vinda do site !
 Algum vistante mandou essa mensagem pelo site.
-Nome: $nome
-Email: $email
-Mensagem: $texto";
+Nome: ".$nome." 
+Email: ".$email." 
+Mensagem: ".$texto;
 
-mail("$Destinatario","$Titulo", "$mensagem1","From:$email");
+mail($Destinatario,$titulo, $mensagem1,"From:". $email);
 ?>
 <html>
 <body>
